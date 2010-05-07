d4934eb6870f36e04a307fa5ec6e6e2dd250e244
keiono <keiono@bac3df7d-1e23-83cd-b5da-1fcb26fd12d5>
2010-05-07 23:09:41 +0000

    Unit test added.  Tests should use JUnit 4 style.
    git-svn-id: https://kgmlreader.googlecode.com/svn/trunk@18 bac3df7d-1e23-83cd-b5da-1fcb26fd12d5

---
+++ test/org/cytoscape/data/reader/kgml/test/KGMLReaderTest.java
@@ -0,0 +1,39 @@
+package org.cytoscape.data.reader.kgml.test;
+
+import static org.junit.Assert.*;
+
+import org.cytoscape.data.reader.kgml.KGMLReader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KGMLReaderTest {
+	
+	private KGMLReader reader1;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRead() throws Exception {
+		reader1 = new KGMLReader("testData/bsu00010.xml");
+		reader1.read();
+		
+		final int[] nodeArray = reader1.getNodeIndicesArray();
+	
+		assertNotNull(nodeArray);
+	
+		//assertEquals(NUMBE of NODES, nodeArray.length);
+		
+		
+		final int[] edgeArray = reader1.getEdgeIndicesArray();
+		
+		assertNotNull(edgeArray);
+	}
+
+}
