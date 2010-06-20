c07898b5c131f766decabdfd99c34dda58782685
keiono <keiono@bac3df7d-1e23-83cd-b5da-1fcb26fd12d5>
2010-06-20 03:46:09 +0000

    Open browser function added.
    git-svn-id: https://kgmlreader.googlecode.com/svn/trunk@89 bac3df7d-1e23-83cd-b5da-1fcb26fd12d5

--- KGMLReader/src/org/cytoscape/kegg/browser/KEGGNodeContextMenuListener.java
+++ KGMLReader/src/org/cytoscape/kegg/browser/KEGGNodeContextMenuListener.java
@@ -1,5 +1,7 @@
 package org.cytoscape.kegg.browser;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -22,13 +24,17 @@ import ding.view.NodeContextMenuListener;
  * Add context menu for KEGG pathways
  * 
  * @author kono
- *
+ * 
  */
 public class KEGGNodeContextMenuListener implements NodeContextMenuListener {
 	private static final String COMPOUND_URL = "http://www.kegg.jp/Fig/compound/";
 	private static final String REACTION_URL = "http://www.kegg.jp/Fig/reaction_small/";
 	private static final String MAP_URL = "http://www.genome.jp/tmp/pathway_thumbnail/";
-	
+
+	private static final String COMPOUND_DBGET_URL = "http://www.genome.jp/dbget-bin/www_bget?cpd:";
+	private static final String REACTION_DBGET_URL = "http://www.genome.jp/dbget-bin/www_bget?";
+	private static final String MAP_DBGET_URL = "http://www.genome.jp/dbget-bin/www_bget?pathway+";
+
 	private CyAttributes nodeAttr = Cytoscape.getNodeAttributes();
 	private final CyNetworkView view;
 
@@ -42,54 +48,80 @@ public class KEGGNodeContextMenuListener implements NodeContextMenuListener {
 			return;
 
 		final JMenu keggMenu = new JMenu("KEGG Options");
-		
-		final String attrValue = nodeAttr.getStringAttribute(nv.getNode().getIdentifier(), "KEGG.entry");
-		if (attrValue == null) return;
-		
+
+		final String attrValue = nodeAttr.getStringAttribute(nv.getNode()
+				.getIdentifier(), "KEGG.entry");
+		if (attrValue == null)
+			return;
+
 		final KEGGEntryType entryType = KEGGEntryType.getType(attrValue);
-		
-		if (entryType == null) return;
-		
-		JMenuItem item = new JMenuItem(); 
-		
-		if(entryType.equals(KEGGEntryType.COMPOUND)) {
+
+		if (entryType == null)
+			return;
+
+		JMenuItem item = new JMenuItem();
+
+		if (entryType.equals(KEGGEntryType.COMPOUND)) {
 			try {
-				final String compoundID = nodeAttr.getStringAttribute(nv.getNode().getIdentifier(), "KEGG.label");
+				final String compoundID = nodeAttr.getStringAttribute(nv
+						.getNode().getIdentifier(), "KEGG.label");
 				URL image = new URL(COMPOUND_URL + compoundID + ".gif");
 				item.setIcon(new ImageIcon(image));
 				item.setText("Compound: " + compoundID);
+				item.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						cytoscape.util.OpenBrowser.openURL(COMPOUND_DBGET_URL
+								+ compoundID);
+					}
+				});
 			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		} else if (entryType.equals(KEGGEntryType.MAP)) {
-			final String mapID = nodeAttr.getStringAttribute(nv.getNode().getIdentifier(), "KEGG.name").split(":")[1];
+			final String mapID = nodeAttr.getStringAttribute(
+					nv.getNode().getIdentifier(), "KEGG.name").split(":")[1];
 			URL image = null;
 			try {
 				image = new URL(MAP_URL + mapID + ".png");
+				item.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						cytoscape.util.OpenBrowser.openURL(MAP_DBGET_URL
+								+ mapID);
+					}
+				});
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
 			item.setIcon(new ImageIcon(image));
 			item.setText("Pathway: " + mapID);
-		} else if (entryType.equals(KEGGEntryType.GENE) || entryType.equals(KEGGEntryType.ORTHOLOG)) {
+		} else if (entryType.equals(KEGGEntryType.GENE)
+				|| entryType.equals(KEGGEntryType.ORTHOLOG)) {
 			try {
-				String reactionID = nodeAttr.getStringAttribute(nv.getNode().getIdentifier(), "KEGG.reaction");
-				URL image = new URL(REACTION_URL + reactionID.split(":")[1] + ".gif");
+				final String reactionID = nodeAttr.getStringAttribute(nv.getNode()
+						.getIdentifier(), "KEGG.reaction");
+				URL image = new URL(REACTION_URL + reactionID.split(":")[1]
+						+ ".gif");
 				item.setIcon(new ImageIcon(image));
 				item.setText("Reaction: " + reactionID);
+				item.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						cytoscape.util.OpenBrowser.openURL(REACTION_DBGET_URL+ reactionID);
+					}
+				});
+				
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		menu.addSeparator();
 		menu.add(keggMenu);
 		menu.add(item);
 	}
-	
-	
 
 }
