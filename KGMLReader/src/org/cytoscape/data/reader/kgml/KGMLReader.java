b69892b4556f89f5a8a853483c4108156e7c185c
Kozo.Nishida <Kozo.Nishida@bac3df7d-1e23-83cd-b5da-1fcb26fd12d5>
2010-07-07 08:34:26 +0000

    don't use TogoWS REST service when reading global map
    git-svn-id: https://kgmlreader.googlecode.com/svn/trunk@99 bac3df7d-1e23-83cd-b5da-1fcb26fd12d5

--- KGMLReader/src/org/cytoscape/data/reader/kgml/KGMLReader.java
+++ KGMLReader/src/org/cytoscape/data/reader/kgml/KGMLReader.java
@@ -22,24 +22,24 @@ import cytoscape.util.URLUtil;
 public class KGMLReader extends AbstractGraphReader {
 
 	private static final String PACKAGE_NAME = "org.cytoscape.data.reader.kgml.generated";
-	
+
 	static final String NETWORK_TYPE = "network type";
 	static final String NETWORK_TYPE_VALUE = "KEGG Pathway";
 	static final String SPECIES = "KEGG.org";
-	static final String NUMBER= "KEGG.number";
+	static final String NUMBER = "KEGG.number";
 	static final String IMAGE = "KEGG.image";
 	static final String LINK = "KEGG.link";
 	static final String TITLE = "KEGG.title";
 
 	private URL targetURL;
-	
+
 	private int[] nodeIdx;
 	private int[] edgeIdx;
-	
+
 	private String networkName;
-	
+
 	private PathwayMapper mapper;
-	
+
 	private Pathway pathway;
 
 	public KGMLReader(final String fileName) {
@@ -55,7 +55,7 @@ public class KGMLReader extends AbstractGraphReader {
 	@Override
 	public void doPostProcessing(CyNetwork network) {
 		mapper.updateView(network);
-		
+
 		// Set network Attr
 		this.mapNetwork(network);
 	}
@@ -79,7 +79,7 @@ public class KGMLReader extends AbstractGraphReader {
 	public void read() throws IOException {
 		InputStream is = null;
 		pathway = null;
-		
+
 		try {
 			final JAXBContext jaxbContext = JAXBContext.newInstance(
 					PACKAGE_NAME, this.getClass().getClassLoader());
@@ -87,8 +87,9 @@ public class KGMLReader extends AbstractGraphReader {
 
 			is = URLUtil.getBasicInputStream(targetURL);
 			pathway = (Pathway) unmarshaller.unmarshal(is);
-			networkName = CyNetworkNaming.getSuggestedNetworkTitle(pathway.getTitle());
-			
+			networkName = CyNetworkNaming.getSuggestedNetworkTitle(pathway
+					.getTitle());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 
@@ -98,33 +99,47 @@ public class KGMLReader extends AbstractGraphReader {
 				is.close();
 			}
 		}
-			
+
 		mapper = new PathwayMapper(pathway);
 		mapper.doMapping();
 		nodeIdx = mapper.getNodeIdx();
 		edgeIdx = mapper.getEdgeIdx();
 
 	}
-	
+
 	/**
-	 *  Store general pathway information as network attribute
+	 * Store general pathway information as network attribute
 	 */
 	private void mapNetwork(final CyNetwork network) {
 		final CyAttributes netAttr = Cytoscape.getNetworkAttributes();
-		netAttr.setAttribute(network.getIdentifier(), PathwayMapper.KEGG_NAME, pathway.getName());
-		netAttr.setAttribute(network.getIdentifier(), SPECIES, pathway.getOrg());
-		netAttr.setAttribute(network.getIdentifier(), NUMBER, pathway.getNumber());
-		netAttr.setAttribute(network.getIdentifier(), IMAGE, pathway.getImage());
+		netAttr.setAttribute(network.getIdentifier(), PathwayMapper.KEGG_NAME,
+				pathway.getName());
+		netAttr
+				.setAttribute(network.getIdentifier(), SPECIES, pathway
+						.getOrg());
+		netAttr.setAttribute(network.getIdentifier(), NUMBER, pathway
+				.getNumber());
+		netAttr
+				.setAttribute(network.getIdentifier(), IMAGE, pathway
+						.getImage());
 		netAttr.setAttribute(network.getIdentifier(), LINK, pathway.getLink());
-		netAttr.setAttribute(network.getIdentifier(), TITLE, pathway.getTitle());
-		netAttr.setAttribute(network.getIdentifier(), NETWORK_TYPE, NETWORK_TYPE_VALUE);
-		
-		try {
-			KEGGRestClient.getCleint().importAnnotation(pathway.getOrg() + pathway.getNumber(), network);
-		} catch (IOException e) {
-			
-			e.printStackTrace();
+		netAttr
+				.setAttribute(network.getIdentifier(), TITLE, pathway
+						.getTitle());
+		netAttr.setAttribute(network.getIdentifier(), NETWORK_TYPE,
+				NETWORK_TYPE_VALUE);
+
+		if (!pathway.getNumber().equals("01100")
+				&& !pathway.getNumber().equals("01110")) {
+			try {
+				KEGGRestClient.getCleint().importAnnotation(
+						pathway.getOrg() + pathway.getNumber(), network);
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
 		}
+
 	}
 
 }
