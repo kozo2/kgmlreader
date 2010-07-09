51366b9e4c2e7fa5aafd26eec77cca562fa53b0e
Kozo.Nishida <Kozo.Nishida@bac3df7d-1e23-83cd-b5da-1fcb26fd12d5>
2010-07-09 05:41:37 +0000

    added disease and dblink map method
    git-svn-id: https://kgmlreader.googlecode.com/svn/trunk@103 bac3df7d-1e23-83cd-b5da-1fcb26fd12d5

--- KGMLReader/src/org/cytoscape/kegg/webservice/KEGGResponseParser.java
+++ KGMLReader/src/org/cytoscape/kegg/webservice/KEGGResponseParser.java
@@ -23,7 +23,7 @@ public class KEGGResponseParser {
 		final List<String> moduleIDs = new ArrayList<String>();
 
 		for (final String module : modules.split("\t")) {
-			
+
 			moduleIDs.add(module.split("  ")[0]);
 
 			System.out.println("--------Module: " + module.split("  ")[0]
@@ -48,6 +48,52 @@ public class KEGGResponseParser {
 
 		attr.setListAttribute(network.getIdentifier(), "KEGG.relpathwayID",
 				relpathwayIDs);
+
+	}
+
+	public void mapDisease(String diseases, CyNetwork network) {
+		final List<String> diseaseIDs = new ArrayList<String>();
+
+		for (String disease : diseases.split("\t")) {
+			diseaseIDs.add(disease.split("  ")[0]);
+		}
+
+		attr.setListAttribute(network.getIdentifier(), "KEGG.diseaseID",
+				diseaseIDs);
+		
+	}
+
+	public void mapDblink(String dblinks, CyNetwork network) {
+		if (dblinks.split("\t").length == 2) {
+			final List<String> umbbdIDs = new ArrayList<String>();
+			final List<String> goIDs = new ArrayList<String>();
+			for (String umbbdID : dblinks.split("\t")[0].split(": ")[1]
+					.split(" ")) {
+				umbbdIDs.add(umbbdID);
+			}
+			for (String goID : dblinks.split("\t")[1].split(": ")[1].split(" ")) {
+				goIDs.add(goID);
+			}
+			attr.setListAttribute(network.getIdentifier(), "UMBBD.dblinks",
+					umbbdIDs);
+			attr.setListAttribute(network.getIdentifier(), "GO.dblinks", goIDs);
+		} else if (dblinks.split("\t").length == 1) {
+			if (dblinks.split("UMBBD: ").length == 2) {
+				final List<String> umbbdIDs = new ArrayList<String>();
+				for (String umbbdID : dblinks.split("UMBBD: ")[1].split(" ")) {
+					umbbdIDs.add(umbbdID);
+				}
+				attr.setListAttribute(network.getIdentifier(), "UMBBD.dblinks",
+						umbbdIDs);
+			} else if (dblinks.split("GO: ").length == 2) {
+				final List<String> goIDs = new ArrayList<String>();
+				for (String goID : dblinks.split("GO: ")[1].split(" ")) {
+					goIDs.add(goID);
+				}
+				attr.setListAttribute(network.getIdentifier(), "GO.dblinks",
+						goIDs);
+			}
+		}
 
 	}
 
